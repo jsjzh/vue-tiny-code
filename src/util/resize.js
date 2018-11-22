c4fa117448d71f36625a2ba1a6a672ff7daf0753
jsjzh <kimimi_king@163.com>
2018-11-22 11:12:09 +0800

    fix

--- src/util/resize.js
+++ src/util/resize.js
@@ -1,9 +1,9 @@
 /*
  * @Author: jsjzh
  * @Email: kimimi_king@163.com
- * @Date: 2018-11-22 10:12:19
+ * @Date: 2018-11-22 10:42:19
  * @LastEditors: jsjzh
- * @LastEditTime: 2018-11-22 11:07:53
+ * @LastEditTime: 2018-11-22 11:11:59
  * @Description: resize 窗口函数调用
  * @use
  *  addResizeListener(element, fn)
@@ -23,8 +23,8 @@ function resizeHandler(entries) {
   }
 }
 
-export function addResizeListener(element, fn) {
-  if (isServer || (!element && fn)) return;
+export const addResizeListener = function(element, fn) {
+  if (isServer || !element || !fn) return;
   if (!element[listenerCtx]) {
     element[listenerCtx] = [];
     element[observerCtx] = new ResizeObserver(resizeHandler);
@@ -33,8 +33,8 @@ export function addResizeListener(element, fn) {
   element[listenerCtx].push(fn);
 }
 
-export function removeResizeListener(element, fn) {
-  if (!isServer || !element || !element[listenerCtx]) return;
+export const removeResizeListener = function(element, fn) {
+  if (isServer || !element || !element[listenerCtx]) return;
   element[listenerCtx].splice(element[listenerCtx].indexOf(fn), 1);
-  !element[listenerCtx].length && element[observerCtx].disconnect()
+  !element[listenerCtx].length && element[observerCtx].disconnect();
 }
