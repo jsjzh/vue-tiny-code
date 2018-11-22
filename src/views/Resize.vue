28b4936e2e07f298e13826d06305b04641baf8fd
jsjzh <kimimi_king@163.com>
2018-11-22 17:48:51 +0800

    add

--- src/views/Resize.vue
+++ src/views/Resize.vue
@@ -1,30 +1,28 @@
 <template>
-  <div ref="demo">
-    <div style="width: 100px;float: left">123</div>
+  <div class="container">
+    <span v-for="i in 100" :key="i">{{i}}</span>
+    <div v-for="i in 100" :key="i">{{i}}</div>
   </div>
 </template>
 
 
 <script>
-import { addResizeListener, removeResizeListener } from "@/util/resize-event";
-
 export default {
   name: "resize",
-  methods: {
-    demo() {
-      console.log("resize");
-    }
-  },
-  mounted() {
-    let { demo } = this.$refs;
-    addResizeListener(demo, this.demo);
-
-    setTimeout(() => {
-      removeResizeListener(demo, this.demo);
-    }, 5000);
-  }
+  methods: {},
+  mounted() {}
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: wheat;
+  width: 500px;
+  height: 500px;
+  overflow: scroll;
+}
 </style>
