be8f95f805bc8bb48fabc0ec8a4399e71de94f94
jsjzh <kimimi_king@163.com>
2019-03-12 11:53:12 +0800

    添加首页导航

--- src/layout/home/index.vue
+++ src/layout/home/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home-container">
+    <el-menu mode="horizontal" :router="true">
+      <el-menu-item index="1" :route="{path: '/'}">首页</el-menu-item>
+      <el-menu-item index="2" :route="{path: '/dragReport'}">仿 Chrome 调色板</el-menu-item>
+      <el-menu-item index="3" :route="{path: '/palette'}">Vue 拖拽报表</el-menu-item>
+    </el-menu>
     <router-view/>
   </div>
 </template>
