d0527b17be61b15a57e65c2720b246387c84cac0
jsjzh <kimimi_king@163.com>
2019-03-15 15:39:51 +0800

    添加拖拽_elementDialogog_的边界情况处理，限定在屏幕内拖动

--- src/directive/dragDialog/dragDialog.js
+++ src/directive/dragDialog/dragDialog.js
@@ -5,11 +5,14 @@ export default {
     const header = el.querySelector('.el-dialog__header')
     const dialog = el.querySelector('.el-dialog')
     header.style.cssText += ';cursor:move;'
-    dialog.style.cssText += ';top:0px;margin: 0;'
+    dialog.style.cssText += ';top:0px;margin:0;'
 
     function handleMousedown(e) {
       let left = e.clientX
       let oldLeft = dialog.style.left
+
+      let maxLeft = window.innerWidth - dialog.offsetWidth
+      let maxTop = window.innerHeight - dialog.offsetHeight
 
       let top = e.clientY
       let oldTop = dialog.style.top
@@ -18,13 +21,19 @@ export default {
       if (oldTop.indexOf('px') !== -1) oldTop = +oldTop.replace('px', '')
 
       function handleMousemove(e) {
-        dialog.style.left = oldLeft + e.clientX - left + 'px'
-        dialog.style.top = oldTop + e.clientY - top + 'px'
+        let moveX = oldLeft + e.clientX - left
+        moveX = moveX >= 0 ? moveX : 0
+        dialog.style.left = Math.min(moveX, maxLeft) + 'px'
+        let moveY = oldTop + e.clientY - top
+        moveY = moveY >= 0 ? moveY : 0
+        dialog.style.top = Math.min(moveY, maxTop) + 'px'
       }
+
       function handleMouseup(e) {
         off(document, 'mousemove', handleMousemove)
         off(document, 'mouseup', handleMouseup)
       }
+
       on(document, 'mousemove', handleMousemove)
       on(document, 'mouseup', handleMouseup)
     }
