12a2a65a50fa59ef259321b7926ab00e69a4f36e
jsjzh <kimimi_king@163.com>
2019-03-15 17:08:12 +0800

    完善拖动_elementDialog_组件

--- src/directive/dragDialog/dragDialog.js
+++ src/directive/dragDialog/dragDialog.js
@@ -1,35 +1,41 @@
 import { on, off } from '@/utils/dom'
 
+const context = '@@dragDialogContext'
+
 export default {
   bind(el, binding, vnode) {
     const header = el.querySelector('.el-dialog__header')
     const dialog = el.querySelector('.el-dialog')
     header.style.cssText += ';cursor:move;'
-    dialog.style.cssText += ';top:0px;margin:0;'
+    dialog.style.cssText += ';top:0px;'
 
     function handleMousedown(e) {
-      let left = e.clientX
-      let oldLeft = dialog.style.left
+      document.documentElement.style.cssText += ';user-select:none;'
 
       let maxLeft = window.innerWidth - dialog.offsetWidth
+      let startLeft = e.clientX
+      let oldLeft = dialog.offsetLeft
+
       let maxTop = window.innerHeight - dialog.offsetHeight
+      let startTop = e.clientY
+      let oldTop = dialog.offsetTop
 
-      let top = e.clientY
-      let oldTop = dialog.style.top
-
-      if (oldLeft.indexOf('px') !== -1) oldLeft = +oldLeft.replace('px', '')
-      if (oldTop.indexOf('px') !== -1) oldTop = +oldTop.replace('px', '')
+      dialog.style.cssText += `;margin:0;left:${oldLeft}px;top:${oldTop}px;`
 
       function handleMousemove(e) {
-        let moveX = oldLeft + e.clientX - left
-        moveX = moveX >= 0 ? moveX : 0
-        dialog.style.left = Math.min(moveX, maxLeft) + 'px'
-        let moveY = oldTop + e.clientY - top
-        moveY = moveY >= 0 ? moveY : 0
-        dialog.style.top = Math.min(moveY, maxTop) + 'px'
+        let moveX = oldLeft + e.clientX - startLeft
+        let currLeft = (dialog.style.left = Math.min(Math.max(0, moveX), maxLeft) + 'px')
+        let moveY = oldTop + e.clientY - startTop
+        let currTop = (dialog.style.top = Math.min(Math.max(0, moveY), maxTop) + 'px')
+
+        dialog.style.cssText += `;left:${currLeft};top:${currTop};`
+
+        // emit onDrag event
+        vnode.child.$emit('dragDialog')
       }
 
       function handleMouseup(e) {
+        document.documentElement.style.cssText += ';user-select:auto;'
         off(document, 'mousemove', handleMousemove)
         off(document, 'mouseup', handleMouseup)
       }
@@ -39,5 +45,19 @@ export default {
     }
 
     on(header, 'mousedown', handleMousedown)
+
+    if (!el[context]) {
+      el[context] = {
+        removeHandle: handleMousedown
+      }
+    } else {
+      el[context].removeHandle = handleMousedown
+    }
+  },
+  unbind(el, binding, vnode) {
+    const header = el.querySelector('.el-dialog__header')
+    off(header, 'mousedown', el[context].removeHandle)
+    el[context] = null
+    delete el[context]
   }
 }
