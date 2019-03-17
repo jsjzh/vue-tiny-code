6485fafaa0b448f5e4523dfbf6d2e37d3ca6782e
jsjzh <kimimi_king@163.com>
2019-03-17 17:23:04 +0800

    增加_customLoading_el_上下文环境

--- src/directive/customLoading/customLoading.js
+++ src/directive/customLoading/customLoading.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import customLoading from '@/components/custom-loading'
 
+const context = '@@loadingContext'
 const Mask = Vue.extend(customLoading)
 
 const toggleLoading = (el, binding) => {
@@ -9,18 +10,17 @@ const toggleLoading = (el, binding) => {
       insertDom(document.body, el, binding)
     }
   } else {
-    el.instance.visible = false
+    el[context].instance.visible = false
   }
 }
 
 const insertDom = (parent, el, binding) => {
-  el.instance.visible = true
-  parent.appendChild(el.mask)
+  el[context].instance.visible = true
+  parent.appendChild(el[context].mask)
 }
 
 export default {
   bind: function(el, binding, vnode) {
-    debugger
     const mask = new Mask({
       el: document.createElement('div'),
       data() {
@@ -29,9 +29,15 @@ export default {
         }
       }
     })
-    console.log(mask)
-    el.instance = mask
-    el.mask = mask.$el
+    if (!el[context]) {
+      el[context] = {
+        instance: mask,
+        mask: mask.$el
+      }
+    } else {
+      el[context].instance = mask
+      el[context].mask = mask.$el
+    }
     binding.value && toggleLoading(el, binding)
   },
   update: function(el, binding) {
@@ -40,6 +46,8 @@ export default {
     }
   },
   unbind: function(el, binding) {
-    el.instance && el.instance.$destroy()
+    el[context].instance && el[context].instance.$destroy()
+    el[context] = null
+    delete el[context]
   }
 }
