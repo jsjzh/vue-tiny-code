b4e0dff0a8c2ae87affc5191cbafb8230ede6e78
jsjzh <kimimi_king@163.com>
2019-05-13 09:26:25 +0800

    edit

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   configureWebpack: config => {
     config.devtool = 'eval-source-map'
-    // TODO 想办法引入 svg 的文件，参考 vue-element-admin 的项目
-    // config.module.rules.push({
-    //   test: /\.svg$/,
-    //   loader: 'svg-sprite-loader',
-    //   include: [resolve('@/icons')],
-    //   options: {
-    //     symbolId: 'icon-[name]'
-    //   }
-    // })
   }
 }
